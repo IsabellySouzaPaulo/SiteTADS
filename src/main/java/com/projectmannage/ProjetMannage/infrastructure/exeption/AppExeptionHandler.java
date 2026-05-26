47ca41c243d3ccacdd448efc0d4d500ae10b5c6b
IsabellySouzaPaulo <isabellysouzapaulo@gmail.com>
2026-05-25 22:08:16 -0400

    Aula do dia 25 de maio 2026

--- src/main/java/com/projectmannage/ProjetMannage/infrastructure/exeption/AppExeptionHandler.java
+++ src/main/java/com/projectmannage/ProjetMannage/infrastructure/exeption/AppExeptionHandler.java
@@ -3,26 +3,42 @@ package com.projectmannage.ProjetMannage.infrastructure.exeption;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+@ControllerAdvice
 
 public class AppExeptionHandler extends ResponseEntityExceptionHandler {
-    @ExceptionHandler(value = Exception.class)
-    public ResponseEntity<Object> handleGenericExeption(Exception ex, WebRequest request) {
-        ServletWebRequest servletWebRequest = (ServletWebRequest) request;
 
+    @ExceptionHandler(value = ResquestExeption.class)
+    public ResponseEntity<Object> handleRequestException(ResquestExeption ex, WebRequest request) {
+        return handleException(ex, ex.getErrorCode(), ex.getMessage(), HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<Object> handleGenericException(Exception ex, WebRequest request) {
+        return handleException(ex, null, ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+    }
+    private ResponseEntity<Object> handleException(
+            Exception ex,
+            String errorCode,
+            String message,
+            HttpStatus status,
+            WebRequest request
+    ){
+        ServletWebRequest servletWebRequest = (ServletWebRequest) request;
         return handleExceptionInternal(
                 ex,
                 new RestError(
                         "XYZ",
                         ex.getMessage(),
-                        HttpStatus.BAD_REQUEST.value(),
+                        HttpStatus.INTERNAL_SERVER_ERROR.value(),
                         servletWebRequest.getRequest().getRequestURI()
                 ),
                 new HttpHeaders(),
-                HttpStatus.BAD_REQUEST,
+                HttpStatus.INTERNAL_SERVER_ERROR,
                 request
         );
     }
